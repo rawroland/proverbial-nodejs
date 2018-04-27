9cb16af9ca1e99140fcb7118171dd7ef9d48a1af
Roland Awemo <awemo@publicplan.de>
2018-04-27 14:43:17 +0200

    :green_heart: Add integration tests for adding proverbs

--- tests/integration/src/proverbs/routes.test.js
+++ tests/integration/src/proverbs/routes.test.js
@@ -19,4 +19,18 @@ describe('Proverb routes', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({data: fixtures.proverbs});
   });
+
+  it('adds a new proverb', async () => {
+    const proverb = {
+      title: 'Auf dem Holzweg sein',
+      meaning: 'Sich verirren, Umwege machen, ein Ziel nicht richtig oder ein falsches Ziel ansteuern.'
+    };
+    const response = await request(app).post('/proverbs').send(proverb);
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.data.id).toBeDefined();
+    expect(response.body.data.title).toBe('Auf dem Holzweg sein');
+    expect(response.body.data.meaning)
+      .toBe('Sich verirren, Umwege machen, ein Ziel nicht richtig oder ein falsches Ziel ansteuern.');
+  });
 });
