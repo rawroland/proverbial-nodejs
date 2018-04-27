d3612846c6c8dd4a96d3fd63c473c38a7eef2398
Roland Awemo <awemo@publicplan.de>
2018-04-27 14:48:43 +0200

    :green_heart: Add unit tests for creating proverbs

--- tests/unit/src/proverbs/repository.test.js
+++ tests/unit/src/proverbs/repository.test.js
@@ -1,6 +1,9 @@
-const {getAll, createClient} = require('../../../../src/proverbs/repository');
+const {getAll, create, getById, createClient} = require('../../../../src/proverbs/repository');
 const {resetFixtures, fixtures} = require('../../../setup');
+const {translateWithAlternatives} = require('deepl-translator');
+const {create: createTranslator} = require('../../../../src/translator');
 
+const translator = createTranslator(translateWithAlternatives);
 const client = createClient();
 
 afterEach(() => {
@@ -12,5 +15,38 @@ describe('Proverb repository', () => {
     const proverbs = await getAll(client);
 
     expect(proverbs).toEqual(fixtures.proverbs);
+  });
+
+  it('retrieves a proverb by id', async () => {
+    const proverb = await getById(fixtures.proverbs[0].id, client);
+
+    expect(proverb).toEqual(fixtures.proverbs[0]);
+  });
+
+  it('creates a new proverb', async () => {
+    let proverbs = await getAll(client);
+    expect(proverbs).toHaveLength(2);
+
+    const proverb = {
+      title: 'Auf dem Holzweg sein',
+      meaning: 'Sich verirren, Umwege machen, ein Ziel nicht richtig oder ein falsches Ziel ansteuern.'
+    };
+    const saved = await create(proverb, client, translator);
+
+    expect(saved.id).toBeDefined();
+    expect(saved.title).toBe('Auf dem Holzweg sein');
+    expect(saved.meaning)
+      .toBe('Sich verirren, Umwege machen, ein Ziel nicht richtig oder ein falsches Ziel ansteuern.');
+    expect(saved.translations).toEqual([
+      'be barking up the wrong tree',
+      'be barking up the wrong tree',
+      'be off the track', 'Barking up the wrong tree',
+      'bark up the wrong tree',
+      'be on the wrong track',
+      'get hold of the wrong end of the stick',
+      'Get off the wrong track'
+    ]);
+    proverbs = await getAll(client);
+    expect(proverbs).toHaveLength(3);
   });
 });
